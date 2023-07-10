45a55777183dc23cd0e779f829fb7145100d82d6
Ally-Matias <allyquison.matias@gmail.com>
2023-07-10 18:05:37 -0300

    atualizando o front

--- Frontend/src/hooks/useAuth.js
+++ Frontend/src/hooks/useAuth.js
@@ -1,61 +1,66 @@
 import api from '../utils/api'
 
-import { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
 
-export default function useAuth() {
-  const [authenticated, setAuthenticated] = useState(false)
-  const history = useHistory()
+export default function useAuth(){
+    const [authenticated, setAuthenticated] = useState(false)
+    const history = useHistory()
 
-  useEffect(() => {
-    const token = localStorage.getItem('token')
+    useEffect(() =>{
 
-    if (token) {
-      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+        const token = localStorage.getItem('token')
 
-      setAuthenticated(true)
+        if(token){
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+
+            setAuthenticated(true)
+        }
+
+    }, [])
+
+
+    async function SignUp(user){
+        try{
+            const data = await api.post('/users/register', user).then((response) =>{
+                return response.data
+            })
+            alert(data.message)
+            await authUser(data)
+        } catch(error){
+            // tratar erro
+            alert(error.response.data.message)
+            console.log(error)
+        }
     }
-  }, [])
 
-  async function SignUp(user) {
-    try {
-      const data = await api.post('/users/register', user).then((response) => {
-        return response.data
-      })
-
-      await authUser(data)
-    } catch (error) {
-      // tratar erro
-      console.log(error)
+    async function login(user){
+        try {
+            const data = await api.post('/users/login', user).then((response) =>{
+                return response.data
+            })
+            alert(data.message)
+            await authUser(data)
+        } catch (error) {
+            alert(error.response.data.message)
+            console.log(error)
+        }
     }
-  }
 
-  async function login(user) {
-    try {
-      const data = await api.post('/users/login', user).then((response) => {
-        return response.data
-      })
+    async function authUser(data){
+        setAuthenticated(true)
 
-      await authUser(data)
-    } catch (error) {
-      console.log(error)
+        localStorage.setItem('token', JSON.stringify(data.token))
+
+        window.location.href = '/';
     }
-  }
 
-  async function authUser(data) {
-    setAuthenticated(true)
+    function logout(){
+        setAuthenticated(false)
+        localStorage.removeItem("token")
+        api.defaults.headers.Authorization = undefined
+        window.location.href = '/SignIn';
+    }
 
-    localStorage.setItem('token', JSON.stringify(data.token))
-
-    window.location.href = '/'
-  }
-
-  function logout() {
-    setAuthenticated(false)
-    localStorage.removeItem('token')
-    api.defaults.headers.Authorization = undefined
-    window.location.href = '/SignIn'
-  }
-
-  return { authenticated, SignUp, logout, login }
+    return { authenticated, SignUp, logout, login }
 }
