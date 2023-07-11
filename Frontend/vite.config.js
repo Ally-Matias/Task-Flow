714dfff98bbbc6311a8e613f6b7d3b592b1e0a4f
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-07-11 17:06:35 -0300

    fix: corrigido o problema do tls/ssl

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      rejectUnauthorized: false,
+      key: path.resolve(__dirname, 'Certificate/frontend.key'),
+      cert: path.resolve(__dirname, 'Certificate/frontend.crt'),
+    },
+  },
   plugins: [react()],
 })
