bc58ab6c9f2c7657c3ad978dd9ea5e07696cc2ba
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-07-10 21:12:09 -0300

    feito o tratamento dos erros na página home e configurado home para user autenticado

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,21 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from 'react'
 
 import GlobalStyle from './styles/global'
-import SignIn from './pages/SignIn'
-import Home from './pages/Home'
-import SignUp from './pages/SignUp'
 
 import { ToastContainer } from 'react-toastify'
+
+import Routes from './routes'
 
 // context
 import { UserProvider } from './context/UserContext'
 
 function App() {
   return (
-    <Router>
-      <UserProvider>
-        <ToastContainer />
-        <GlobalStyle />
-        <Switch>
-          <Route path="/SignIn">
-            <SignIn />
-          </Route>
-          <Route path="/SignUp">
-            <SignUp />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </UserProvider>
-    </Router>
+    <UserProvider>
+      <ToastContainer />
+      <GlobalStyle />
+      <Routes />
+    </UserProvider>
   )
 }
 
